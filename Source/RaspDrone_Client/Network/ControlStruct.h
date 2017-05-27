31acf717e14c5c1e34a14d225e60d9c18e78b16d
Leandro Ortega <leandro@g-ortega.com.ar>
2017-05-27 16:17:30 -0300

    se agregan los valores que se enviarán al drone

--- Source/RaspDrone_Client/Network/ControlStruct.h
+++ Source/RaspDrone_Client/Network/ControlStruct.h
@@ -8,11 +8,37 @@
 /**
  * 
  */
-UCLASS()
-class RASPDRONE_CLIENT_API UControlStruct : public UUserDefinedStruct {
+USTRUCT(BlueprintType)
+struct FControlStruct
+{
 	GENERATED_BODY()
 	
-	
-	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Control")
+	float Forward;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Control")
+	float Back;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Control")
+	float Left;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Control")
+	float Right;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Control")
+	float TurnLeft;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Control")
+	float TurnRight;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Control")
+	float GoUp;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Control")
+	float GoDown;
+
+	FControlStruct(){
+		Forward = 0;
+		Back = 0;
+		Left = 0;
+		Right = 0;
+		TurnLeft = 0;
+		TurnRight = 0;
+		GoUp = 0;
+		GoDown = 0;
+	}
 	
 };
